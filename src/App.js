1f1abdc0e77ec0e01adf156a29016ea4fa3c8500
Bertil T <bertilt4@gmail.com>
2021-07-11 11:49:00 -0500

    :art: Display UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
+const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
 function App() {
-  console.log("Greeter ABI: ", Greeter.abi);
+  const [greeting, setGreeting] = useState();
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum == 'undefined') return;
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const contract = new ethers.Contract(contractAddress, Greeter.abi, provider)
+    try {
+      const data = await contract.greet()
+      console.log('data: ', data)
+    } catch (err) {
+      console.log("Error: ", err)
+    }
+  }
+
+  async function setCGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum == 'undefined') return;
+    await requestAccount()
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, Greeter.abi, signer)
+    const transaction = await contract.setGreeting(greeting)
+    await transaction.wait()
+    fetchGreeting()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <input onChange={e => setGreeting(e.target.value)} placeholder="Set greeting" />
+        <button onClick={setCGreeting}>Set Greeting</button>
       </header>
     </div>
   );
