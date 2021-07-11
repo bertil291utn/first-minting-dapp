b1e89abb0bf5b50cd87bc57daa876f991ac4f170
Bertil T <bertilt4@gmail.com>
2021-07-11 10:59:37 -0500

    :zap: deployed smart contract

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("Greeter ABI: ", Greeter.abi);
   return (
     <div className="App">
       <header className="App-header">
